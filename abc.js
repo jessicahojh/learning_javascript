132e878e119d3f0b92ddc2c51fd9dd7337554965
Jessica <jessica.ho.jh@gmail.com>
2019-09-10 10:40:48 -0700

    learning more js

---
+++ abc.js
@@ -0,0 +1,34 @@
+let snail = {
+    name: "Jessica",
+    age: 2,
+    sex: 'female'
+};
+
+snail.name = "Kelley"
+console.log(snail.name)
+
+class Snail {
+  constructor(name, age, sex) {
+    this._name = name;
+    this.age = age;
+    this.sex = sex;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName
+  }
+  
+  
+}
+
+let jessica = new Snail("Jessica", 2, "female")
+
+// jessica.name = "Kelley"
+jessica.name= "Kelley"
+console.log(jessica.name)
+
+
