10f7997dcaba80340af487fb82f120d8fc0a5b77
Jessica <jessica.ho.jh@gmail.com>
2019-12-30 18:24:41 -0800

    promises vs callbacks

---
+++ promisesVsCallbacks.js
@@ -0,0 +1,101 @@
+// Simple promise exmaple
+let p = new Promise((resolve, reject) => {
+    let a = 1 + 1
+    if (a == 2) {
+        resolve('Success')
+    } else {
+        reject('Failed')
+    }
+}) 
+
+p.then((message) => {
+    console.log('This is in the then ' + message)
+}).catch((message) => {
+    console.log('This is in the catch ' + message)
+})
+
+
+// Callback version
+function watchTutorialCallback(callback, errorCallback) {
+    let userLeft = false
+    let userWatchingCatMeme = false
+  
+    if (userLeft) {
+      errorCallback({
+        name: 'User Left', 
+        message: ':('
+      })
+    } else if (userWatchingCatMeme) {
+      errorCallback({
+        name: 'User Watching Cat Meme',
+        message: 'WebDevSimplified < Cat' 
+      })
+    } else {
+      callback('Thumbs up and Subscribe')
+    }
+  }
+
+  watchTutorialCallback(message => {
+    console.log(message)
+  }, error => {
+    console.log(error.name + ' ' + error.message)
+  })
+  
+  // Promise version
+  // Promises avoids callback hell (nested callbacks)
+  function watchTutorialPromise() {
+    let userLeft = false
+    let userWatchingCatMeme = false
+    return new Promise((resolve, reject) => {
+      if (userLeft) {
+        reject({
+          name: 'User Left', 
+          message: ':('
+        })
+      } else if (userWatchingCatMeme) {
+        reject({
+          name: 'User Watching Cat Meme',
+          message: 'WebDevSimplified < Cat' 
+        })
+      } else {
+        resolve('Thumbs up and Subscribe')
+      }
+    })
+  }
+  
+  watchTutorialPromise().then(message => {
+    console.log(message)
+  }).catch(error => {
+    console.log(error.name + ' ' + error.message)
+  })
+
+  // Run all promises in parallel
+  // Use Promise.all
+  // sends an array in the end
+  const recordVideoOne = new Promise((resolve, reject) => {
+    resolve('Video 1 Recorded')
+  })
+  
+  const recordVideoTwo = new Promise((resolve, reject) => {
+    resolve('Video 2 Recorded')
+  })
+  
+  const recordVideoThree = new Promise((resolve, reject) => {
+    resolve('Video 3 Recorded')
+  })
+  
+  Promise.all([
+    recordVideoOne,
+    recordVideoTwo,
+    recordVideoThree
+  ]).then(messages => {
+    console.log(messages)
+  })
+  
+  Promise.race([
+    recordVideoOne,
+    recordVideoTwo,
+    recordVideoThree
+  ]).then(message => {
+    console.log(message)
+  })
